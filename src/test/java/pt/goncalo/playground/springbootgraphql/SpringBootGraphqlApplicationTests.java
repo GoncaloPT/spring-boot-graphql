eb7500d91cb917ab8c87e493ae2a7f6ff9290021
GoncaloPT <goncalosamuel@gmail.com>
2022-08-23 17:30:25 +0100

    first

---
+++ src/test/java/pt/goncalo/playground/springbootgraphql/SpringBootGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.goncalo.playground.springbootgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
